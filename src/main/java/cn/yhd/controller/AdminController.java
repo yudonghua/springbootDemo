ff1f95880d30aa3c23be531d401bd97cd2b386e1
yuhuadong <yuhuadong@soulapp.cn>
2019-06-27 20:36:01 +0800

    master

--- src/main/java/cn/yhd/controller/AdminController.java
+++ src/main/java/cn/yhd/controller/AdminController.java
@@ -25,6 +25,7 @@ public class AdminController {
         if(!ADMIN.equals(username)){
 
             return "/static/login";
+
         }
         model.addAttribute("list_user",userService.getAllUser());
         return "/static/admin";
