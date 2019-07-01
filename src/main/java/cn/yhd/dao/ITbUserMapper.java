3cb7756fa3bdf1981a3037d3677a51d279bf6296
yuhuadong <yuhuadong@soulapp.cn>
2019-07-01 13:47:55 +0800

    yhd

---
+++ src/main/java/cn/yhd/dao/ITbUserMapper.java
@@ -0,0 +1,11 @@
+package cn.yhd.dao;
+
+import cn.yhd.base.IBaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Created by PC on 2018/8/26.
+ */
+@Mapper
+public interface ITbUserMapper extends IBaseMapper {
+}
