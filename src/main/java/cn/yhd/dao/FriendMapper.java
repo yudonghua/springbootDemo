37c598e5df85683dc30f80dd57b528753403196a
dongdong <723948277@qq.com>
2019-08-15 16:09:56 +0800

    local

--- src/main/java/cn/yhd/dao/FriendMapper.java
+++ src/main/java/cn/yhd/dao/FriendMapper.java
@@ -24,7 +24,7 @@ import java.util.List;
  */
 @Mapper
 public interface FriendMapper {
-    //find123
+    //find123local
     List<Friend> getFriendByUser(String username);
     List<Friend> getReqByUser(String username);
     void addFriend(Friend friend);
