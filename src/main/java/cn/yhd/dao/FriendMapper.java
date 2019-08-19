4483f8e33323e266c561ce8ef8b1c36e39bec4c1
dongdong <723948277@qq.com>
2019-08-19 15:11:20 +0800

    fixbug

--- src/main/java/cn/yhd/dao/FriendMapper.java
+++ src/main/java/cn/yhd/dao/FriendMapper.java
@@ -26,6 +26,7 @@ import java.util.List;
 public interface FriendMapper {
     //find123localbbbbbb
     List<Friend> getFriendByUser(String username);
+    //bbb
     List<Friend> getReqByUser(String username);
     void addFriend(Friend friend);
     void deleteFriend(Friend friend);
